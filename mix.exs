8bae042463ef5965b0b59942a96182cfd7ce4c7b
Hendrik Voßkamp <hendrik.vosskamp@gmail.com>
2025-04-26 21:11:09 +0200

    Downgrade exqlite to 0.29

--- mix.exs
+++ mix.exs
@@ -35,6 +35,7 @@ defmodule Pomodoro.MixProject do
       {:phoenix, "~> 1.7.21"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
+      {:exqlite, "~> 0.29.0"},
       {:ecto_sqlite3, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
